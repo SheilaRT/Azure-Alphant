4f1320bb4974edab75ddfea2599a218fe47baf6c
Sheila <48502041+SheilaRT@users.noreply.github.com>
2019-07-31 22:36:27 -0700

    Update pua.js

--- pua.js
+++ pua.js
@@ -9,26 +9,26 @@ var codepoints = [
 ];
       
   for (var i = 0; i < codepoints.length; i++) {
-	var myArticle = document.createElement('article');
-	var glyph = codepoints[i].g;
-	var hex = codepoints[i].h;
-    	var myPara1 = document.createElement('p');
-    	var myPara2 = document.createElement('p');
-		  var myPara3 = document.createElement('p');
-		  var myPara4 = document.createElement('p');
-		  var myPara5 = document.createElement('p');
+    var myArticle = document.createElement('article');
+    var glyph = codepoints[i].g;
+    var hex = codepoints[i].h;
+      var myPara1 = document.createElement('p');
+      var myPara2 = document.createElement('p');
+      var myPara3 = document.createElement('p');
+      var myPara4 = document.createElement('p');
+      var myPara5 = document.createElement('p');
 
-    	myPara1.textContent = glyph;
-    	myPara2.textContent = 'U+' + hex;
-		  myPara3.textContent = '&#x' + hex + ';';
-		  myPara4.textContent = '&#' + parseInt(hex, 16) + ';';
-		  myPara5.textContent = '\\' + 'u' + hex;
+      myPara1.textContent = glyph;
+      myPara2.textContent = 'U+' + hex;
+      myPara3.textContent = '&#x' + hex + ';';
+      myPara4.textContent = '&#' + parseInt(hex, 16) + ';';
+      myPara5.textContent = '\\' + 'u' + hex;
 
-    	myArticle.appendChild(myPara1);
-    	myArticle.appendChild(myPara2);
-		  myArticle.appendChild(myPara3);
-		  myArticle.appendChild(myPara4);
-		  myArticle.appendChild(myPara5);
+      myArticle.appendChild(myPara1);
+      myArticle.appendChild(myPara2);
+      myArticle.appendChild(myPara3);
+      myArticle.appendChild(myPara4);
+      myArticle.appendChild(myPara5);
 
     section.appendChild(myArticle);
   }
