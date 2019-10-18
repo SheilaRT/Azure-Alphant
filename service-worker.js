7c97aec25a211153e123a5143cb720b16c741375
Sheila <48502041+SheilaRT@users.noreply.github.com>
2019-10-17 20:59:27 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -93,3 +93,32 @@ const staticAssets=[
     './yijing.html'
     './yijing.js'
 ];
+self.addEventListener('install', async event=>{
+    const cache = await caches.open('static-cache');
+    cache.addAll(staticAssets);
+});
+self.addEventListener('fetch', event => {
+    const req = event.request;
+    const url = new URL(req.url);
+
+    if(url.origin === location.url){
+        event.respondWith(cacheFirst(req));
+    } else {
+        event.respondWith(newtorkFirst(req));
+    }
+});
+async function cacheFirst(req){
+    const cachedResponse = caches.match(req);
+    return cachedResponse || fetch(req);
+};
+async function networkFirst(req){
+    const cache = await caches.open('dynamic-cache');
+
+    try {
+        const res = await fetch(req);
+        cache.put(req, res.clone());
+        return res;
+    } catch (error) {
+        return await cache.match(req);
+    }
+};
